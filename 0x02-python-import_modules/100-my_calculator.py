e70d3c09a470b1101c3ff5933e30691b8cb90fa1
Ami Manye <amimanyeaglago@gmail.com>
2023-10-06 09:54:18 +0000

    add numbers using arguments

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -19,13 +19,13 @@ if __name__ == '__main__':
 
     '''do operation'''
     if op == '+':
-        print('{} {} {} = {}'.format(a, op, b, add(a,b)))
+        print('{} {} {} = {}'.format(a, op, b, add(a, b)))
     elif op == '-':
-        print('{} {} {} = {}'.format(a, op, b, sub(a,b)))
+        print('{} {} {} = {}'.format(a, op, b, sub(a, b)))
     elif op == '*':
-        print('{} {} {} = {}'.format(a, op, b, mul(a,b)))
+        print('{} {} {} = {}'.format(a, op, b, mul(a, b)))
     elif op == '/':
-        print('{} {} {} = {}'.format(a, op, b, sub(a,b)))
+        print('{} {} {} = {}'.format(a, op, b, div(a, b)))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
