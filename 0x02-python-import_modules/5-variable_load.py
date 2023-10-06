38855aab0e5ce3dd9df3df91aa8f3ca8f6bf4982
Ami Manye <amimanyeaglago@gmail.com>
2023-10-06 08:26:05 +0000

    import variable from module

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -3,5 +3,6 @@
 # AUTHOR - Ami Manye
 
 # import variable from a module
-from variable_load_5 import a
-print('{}'.format(a))
+if __name__ == '__main__':
+    from variable_load_5 import a
+    print('{}'.format(a))
