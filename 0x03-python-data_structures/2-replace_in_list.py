fad8017a95b11daa7480ac72d88f4036588c4449
Ami Manye <amimanyeaglago@gmail.com>
2023-10-09 15:13:06 +0000

    replace element in list passed :: edge cases

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,6 +5,10 @@
 def replace_in_list(my_list, idx, element):
     '''replace element at idx in my_list'''
 
+    '''if element is negative'''
+    if idx < 0 or idx >= len(my_list):
+        return
+
     my_list[idx] = element
 
     return my_list
