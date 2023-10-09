cd8fa0e549523f78282a13ff0fc5e0bffa67d624
Ami Manye <amimanyeaglago@gmail.com>
2023-10-09 15:04:09 +0000

    print elements of list passed

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# AUTHOR - Ami Manye
+
+def print_list_integer(my_list=[]):
+    '''print elements of list: my_list'''
+
+    for elem in my_list:
+        print('{}'.format(elem))
