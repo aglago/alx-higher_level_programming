682c98940bb30a5cee334b2455e682508bea2d88
Ami Manye <amimanyeaglago@gmail.com>
2023-10-09 14:58:05 +0000

    get element at index idx :: pycodestyle

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,11 +5,10 @@
 def element_at(my_list, idx):
     '''fetch element at index: idx'''
 
-
     '''if element is negative'''
     if idx < 0 or idx >= len(my_list):
-        return;
+        return
 
     '''print the element'''
     element = my_list[idx]
-    return (element);
+    return (element)
