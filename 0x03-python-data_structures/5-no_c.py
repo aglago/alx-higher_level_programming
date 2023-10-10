c89ff66dfcb337da41e1b9125af0752bf3311e2d
Ami Manye <amimanyeaglago@gmail.com>
2023-10-10 09:46:11 +0000

    build up a string from empty using +

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+
+# AUTHOR - Ami Manye
+
 def no_c(my_string):
-    new_string = my_string.translate({ord(i): None for i in 'cC'})
-    return new_string
+    '''remove all c and C from my_string'''
+
+    new_string = ''
+    for c in my_string:
+        if c != 'c' and c != 'C':
+            new_string += c
+    return (new_string)
