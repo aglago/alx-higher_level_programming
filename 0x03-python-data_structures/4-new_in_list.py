a993eac7d9b3fd38c9d95267299b5f6dcf025f06
Ami Manye <amimanyeaglago@gmail.com>
2023-10-10 09:00:19 +0000

    removed duplicate conditions conflicts

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,14 +6,11 @@ def new_in_list(my_list, idx, element):
     '''replace element at idx without modifying my_list'''
 
     '''if list is None'''
-    if my_list is None or idx < 0 or idx >= len(my_list):
-        return;
-    '''if idx is negative'''
-    if idx < 0 or idx >= len(my_list):
-        return (my_list.copy)
-    '''if idx is out of range'''
     if my_list is None:
         return;
+    '''if idx is negative or out of range'''
+    if idx < 0 or idx >= len(my_list):
+        return (my_list.copy)
 
     new_list = my_list.copy()
     new_list[idx] = element
