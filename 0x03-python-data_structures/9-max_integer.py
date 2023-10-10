553e1c2e7db44390177050fee7d5384667f1b397
Ami Manye <amimanyeaglago@gmail.com>
2023-10-10 11:54:38 +0000

    took care of negative

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,10 +6,10 @@ def max_integer(my_list=[]):
     '''find the largest integer'''
 
     '''if list is empty'''
-    if my_list is None:
+    if my_list is None or len(my_list) == 0:
         return
 
-    largest = 0
+    largest = my_list[0]
     for num in my_list:
         if num > largest:
             largest = num
