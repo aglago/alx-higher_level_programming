339246d8322b0884702bb7bb3edc0297558c4bcd
Ami Manye <amimanyeaglago@gmail.com>
2023-10-10 08:37:23 +0000

    replace element at idx

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,8 +7,8 @@ def replace_in_list(my_list, idx, element):
 
     '''if element is negative'''
     if idx < 0 or idx >= len(my_list):
-        return
+        return (my_list)
 
     my_list[idx] = element
 
-    return my_list
+    return (my_list)
