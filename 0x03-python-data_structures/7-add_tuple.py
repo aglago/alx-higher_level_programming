c3b55bfdc28e4d5aab57817afa801804dc634ccc
Ami Manye <amimanyeaglago@gmail.com>
2023-10-10 11:28:47 +0000

    add tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
+
+# AUTHOR - Ami Manye
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple = ()
-    tuple_1 = tuple_a + (0, 0)
-    tuple_2 = tuple_b + (0, 0)
-    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
-    return new_tuple
+    '''add tuples'''
+
+    '''if tuple is none'''
+    if len(tuple_a) < 2:
+        tuple_a += 0,
+        tuple_a += 0,
+    if len(tuple_b) < 2:
+        tuple_b += 0,
+        tuple_b += 0,
+    '''if tuple is 1'''
+    if len(tuple_a) < 2:
+        tuple_a += 0,
+    if len(tuple_b) < 2:
+        tuple_b += 0,
+
+    '''if tuple is more than 2'''
+    tup = ()
+    tup += tuple_a[0] + tuple_b[0],
+    tup += tuple_a[1] + tuple_b[1],
+
+    return (tup)
+    
