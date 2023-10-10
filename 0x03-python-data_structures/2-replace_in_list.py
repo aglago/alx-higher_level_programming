83baca9eea987d365bdfffbed71812ac0097cd87
Ami Manye <amimanyeaglago@gmail.com>
2023-10-10 08:15:53 +0000

    replace element in list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+
+# AUTHOR - Ami Manye
+
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+    '''replace element at idx in my_list'''
+
+    '''if element is negative'''
+    if idx < 0 or idx >= len(my_list):
+        return
+
+    my_list[idx] = element
+
+    return my_list
