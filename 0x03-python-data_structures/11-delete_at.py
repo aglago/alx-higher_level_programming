43f98684a9683ad65d892673020649616d8a9c33
Ami Manye <amimanyeaglago@gmail.com>
2023-10-10 12:23:04 +0000

    using the del keyword in python

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+
+# AUTHOR - Ami Manye
+
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
-        del my_list[idx]
-    return my_list
+    '''delete item at specific position'''
+
+    '''return nothing for negative index'''
+    '''return nothing for out of range'''
+    if idx < 0 or idx >= len(my_list):
+        return
+
+    del my_list[idx]
+    lst = my_list.copy()
+    return (lst)
