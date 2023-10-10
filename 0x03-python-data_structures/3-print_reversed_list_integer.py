c9626f28fc710800bd9ab6ada529c503e979bd01
Ami Manye <amimanyeaglago@gmail.com>
2023-10-10 08:44:17 +0000

    removed semi colon

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,7 +6,7 @@ def print_reversed_list_integer(my_list=[]):
     '''print list passed in reverse order'''
 
     '''reverse'''
-    idx = len(my_list) - 1;
+    idx = len(my_list) - 1
     while idx >= 0:
         print('{:d}'.format(my_list[idx]))
         idx -= 1
