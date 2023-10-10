ee71cc4cd684b93ea1eea9356e04b4e0b81084e3
Ami Manye <amimanyeaglago@gmail.com>
2023-10-10 08:58:12 +0000

    replace element but do not alter original

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+
+# AUTHOR - Ami Manye
+
 def new_in_list(my_list, idx, element):
-    copy = my_list.copy()
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list.copy()
-    else:
-        copy[idx] = element
-        return copy
+    '''replace element at idx without modifying my_list'''
+
+    '''if list is None'''
+    if my_list is None or idx < 0 or idx >= len(my_list):
+        return;
+    '''if idx is negative'''
+    if idx < 0 or idx >= len(my_list):
+        return (my_list.copy)
+    '''if idx is out of range'''
+    if my_list is None:
+        return;
+
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return (new_list)
