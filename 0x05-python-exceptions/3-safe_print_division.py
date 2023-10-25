dc9d448daec3fa110275b4fff8879ca1077a159d
Ami Manye <amimanyeaglago@gmail.com>
2023-10-25 12:38:52 +0000

    round to a certain decimal place with .1f

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -11,5 +11,7 @@ def safe_print_division(a, b):
         return
     finally:
         if div is not None:
-            print('Inside result: {:f}'.format(div))
+            print('Inside result: {:.1f}'.format(div))
+        else:
+            print('Inside result: ', div)
     return div
