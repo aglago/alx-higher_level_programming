bcc81ad623ad2e65ea45645967812a3e754a7433
Ami Manye <amimanyeaglago@gmail.com>
2023-11-07 16:25:48 +0000

    square class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+# AUTHOR - Ami Manye
+
+'''A Square class'''
 
 
 class Square:
-    """Represent a square."""
-
+    '''This is a class with a private square attribute'''
     def __init__(self, size=0):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new square.
-        """
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise TypeError('size must be an integer')
+        if (size < 0):
+            raise ValueError('size must be >= 0')
         self.__size = size
