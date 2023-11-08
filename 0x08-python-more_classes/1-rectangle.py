4b94a109cc48e524737c76914a185f46d7da127c
Ami Manye <amimanyeaglago@gmail.com>
2023-11-08 15:04:19 +0000

    added instantiating private variables (attribute) and compared value instead of self.__

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@
 class Rectangle:
     '''defines a rectangle'''
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -21,7 +21,7 @@ class Rectangle:
         '''setter function'''
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        elif self.__width < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
@@ -35,6 +35,6 @@ class Rectangle:
         '''setter function'''
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        elif self.__height < 0:
+        elif value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
