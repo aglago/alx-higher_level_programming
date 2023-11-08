52feb358946b8cf2c34534f99923d96de39e9569
Ami Manye <amimanyeaglago@gmail.com>
2023-11-08 15:56:02 +0000

    fixed for if height too is zero

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
 
     def __str__(self):
         '''defines a string representation of the rectangle'''
-        if self.__width == 0:
+        if self.__width == 0 or self.__height == 0:
             print()
             return ''
         rect = ''
