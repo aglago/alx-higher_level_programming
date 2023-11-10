e1b1eef61adb7ac98104305dd791930dbb495a37
Ami Manye <amimanyeaglago@gmail.com>
2023-11-10 12:58:48 +0000

    changed class name to type(self)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,5 +10,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
