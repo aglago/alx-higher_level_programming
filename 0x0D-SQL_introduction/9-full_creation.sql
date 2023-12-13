e05f93104486476aab96cb7d8640c98b94595449
smaglago <amimanyeaglago@gmail.com>
2023-12-13 20:45:31 +0000

    create table and add multiple rows

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- AUTHOR: Samuella M. Aglago
+-- create a table and add multiple rows
+
+CREATE TABLE second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8)
