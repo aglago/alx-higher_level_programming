f421381f9f5afccb85c190d08b60360f653f0424
smaglago <amimanyeaglago@gmail.com>
2023-12-13 20:53:57 +0000

    if not exists

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- AUTHOR: Samuella M. Aglago
 -- create a table and add multiple rows
 
-CREATE TABLE second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
