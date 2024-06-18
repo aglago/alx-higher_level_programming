cd2c0c12a7eccca6002615c786632228f1eacfb2
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-18 23:13:14 +0000

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,19 +2,18 @@
 
 if (process.argv.length <= 3) {
   console.log(0);
-  return;
+} else {
+  const numbers = [];
+
+  for (let i = 2; i < process.argv.length; i++) {
+    numbers.push(+process.argv[i]);
+  }
+
+  const firstMax = Math.max(...numbers);
+
+  const firstMaxIndex = numbers.indexOf(firstMax);
+  numbers.splice(firstMaxIndex, 1);
+
+  const secondMax = Math.max(...numbers);
+  console.log(secondMax);
 }
-
-let numbers = [];
-
-for (let i = 2; i < process.argv.length; i++) {
-  numbers.push(+process.argv[i]);
-}
-
-const firstMax = Math.max(...numbers);
-
-const firstMaxIndex = numbers.indexOf(firstMax);
-numbers.splice(firstMaxIndex, 1);
-
-const secondMax = Math.max(...numbers);
-console.log(secondMax);
