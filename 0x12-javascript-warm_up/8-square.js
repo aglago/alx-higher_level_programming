691eadc7dd760285fa14dd869280938cc152f05e
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-18 20:53:49 +0000

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const len = +process.argv[2];
+
+if (isNaN(len)) {
+  console.log('Missing size');
+} else {
+  let square = '';
+
+  for (let i = 0; i < len; i++) {
+    for (let k = 0; k < len; k++) {
+      square = square + '#';
+    }
+
+    if (i < len - 1) {
+      square = square + '\n';
+    }
+  }
+
+  console.log(square);
+}
