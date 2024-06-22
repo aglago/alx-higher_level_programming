bb854d7c8c2cbfc34e35387712d8feaf5b90a08a
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-22 16:51:55 +0000

    using python to write sql queries

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+hostname = 'localhost'
+username = sys.argv[1]
+password = sys.argv[2]
+database_name = sys.argv[3]
+
+conn = MySQLdb.connect(
+  host=hostname,
+  user=username,
+  passwd=password,
+  db=database_name
+)
+
+cursor = conn.cursor()
+
+query = 'SELECT * FROM states'
+
+cursor.execute(query)
+
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+cursor.close()
+conn.close()
