df55db9035106cfe4fe866e46ba9a4ba79caa82f
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-22 18:36:12 +0000

    list states in database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-This script connects to a MySQL database and retrieves all records from the 'states' table.
+This script connects to a MySQL database
+and retrieves all records from the 'states' table.
 """
 
 import MySQLdb
