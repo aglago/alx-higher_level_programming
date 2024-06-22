7ef34610de5ad64fd619b72a6808eb6b17e586cb
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-22 18:06:19 +0000

    sql with python

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import MySQLdb
-import sys
-
 """
 This script connects to a MySQL database and retrieves all records from the 'states' table.
 """
+
+import MySQLdb
+import sys
