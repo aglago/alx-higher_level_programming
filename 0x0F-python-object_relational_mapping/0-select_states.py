e0c3430e30154231ac385c9690c30c894a9913f5
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-22 18:33:55 +0000

    list states in database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,3 +6,29 @@ This script connects to a MySQL database and retrieves all records from the 'sta
 
 import MySQLdb
 import sys
+
+if __name__ == "__main__":
+    host = "localhost"
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+
+    conn = MySQLdb.connect(
+        host=host,
+        user=user,
+        passwd=passwd,
+        db=db,
+        port=3306
+    )
+
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
