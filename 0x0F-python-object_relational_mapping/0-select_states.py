935a1ece239950cb18bc9a8342583a34042bf402
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-22 18:04:12 +0000

    sql with python

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,26 +2,3 @@
 
 import MySQLdb
 import sys
-
-'''
-This script connects to a
-MySQL database and retrieves
-all records from the 'states'
-table.
-'''
-
-if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
-
-    cursor = conn.cursor()
-
-    # query = 'SELECT * FROM states'
-
-    cursor.execute("SELECT * FROM states")
-
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    conn.close()
