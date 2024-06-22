7c85840d5efab9dc7a176204dcff928640f8bcc1
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-22 19:07:52 +0000

    filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+This script connects to a MySQL database
+and retrieves all records from the 'states' table.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    host = "localhost"
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    search = sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host=host,
+        user=user,
+        passwd=passwd,
+        db=db,
+        port=3306
+    )
+
+    cursor = conn.cursor()
+
+    query = f"SELECT * FROM states WHERE name = '{search}'  ORDER BY id ASC"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
