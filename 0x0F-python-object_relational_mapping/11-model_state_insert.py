c09fd5fdd30655e412760f262e81bf7e7f7cfda3
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-24 02:46:24 +0000

    add a new state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""using sqlalchemy to query data
+from database
+"""
+
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # establishing connection with the database
+    conn_str = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine(conn_str.format(username, password, db_name))
+
+    # database tracker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(session.query(State).filter_by(name="Louisiana").first().id)
