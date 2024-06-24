a166ecae4722c987116c44d4be9b9726138de79e
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-06-24 04:59:05 +0000

    cities in state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from sqlalchemy import (create_engine)
+import sys
+from sqlalchemy.orm import Session
+from model_state import State, Base
+from model_city import City
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+
+    conn_str = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine(conn_str.format(user, passwd, db_name))
+
+    session = Session(bind=engine)
+
+    instances = session.query(State.name, City.id, City.name)\
+        .order_by(City.id).filter(State.id == City.state_id)
+    for instance in instances:
+        print("{}: ({}) {}".format(instance[0], instance[1], instance[2]))
