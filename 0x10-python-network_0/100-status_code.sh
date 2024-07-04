74ff97da3dabcd1c90f18358c4af7881664846d0
amimanye <amimanyeaglago@gmail.com>
2024-07-04 19:05:03 +0000

    add: display status code of response

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a given URL and display the response status code.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# Display status code of response
+curl -s -o /dev/null -w "%{http_code}"
