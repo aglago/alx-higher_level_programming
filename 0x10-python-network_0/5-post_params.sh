bf6d3397544f6d45796b3be0b4963adbb4694b7e
Samuella M. Aglago <samuellamanyeaglago@gmail.com>
2024-07-04 11:34:22 -0700

    fix: add variables, not header variables

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send POST request to URL, send @email and @subject variables along
-curl -sL -X POST -H "email: test@gmail.com" -H "subject: I will always be here for PLD" "$1"
+curl -sL -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
